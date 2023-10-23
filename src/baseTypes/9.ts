940f035bb612cb9d7fb5601a28ee0cd0c861616d
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-10-23 16:48:19 +0300

    hw-TS- basic-type-generic

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,16 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type ObjType = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close' ,
+  details?: {createAt: Date, updateAt: Date}
+  
+}
 
-const page1 = {
+const page1:ObjType = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +21,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2:ObjType = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
